69ae0bb24ba192df03aaf26a7522db1887cc24bd
jpericon <j.pericon@arcanys.com>
2014-12-05 15:34:52 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    api     = require('./api'),
+    users   = require('./accounts'),
+    app     = express();
+
+var path = require('path');
+
+app
+   .use(express.static('./public'))
+   .use(users)
+   .use('/api', api)
+   .get('*', function (req, res) {
+        if (!req.user) {
+            res.redirect('/login');
+        } else {
+            res.sendFile( path.join(__dirname, '/public', 'main.html'));
+        }
+   })
+   .listen(3000);
